37a8cedd7cf193a9411751a93600dba3af5f5e59
terjebh <terje@itfakutetet.no>
2024-05-02 11:09:52 +0200

    Palindrom-eksempel

--- src/no/itfakultetet/Main.java
+++ src/no/itfakultetet/Main.java
@@ -3,15 +3,18 @@ package no.itfakultetet;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
+
         System.out.print("Tast inn en tekst: ");
         Scanner in = new Scanner(System.in);
+
         String tekst = in.nextLine();
         int high = tekst.length() - 1;
         int low = 0;
-        Boolean isPalindrome = true;
-        while (low < high) {
+        boolean isPalindrome = true;
 
+        while (low < high) {
             if (tekst.charAt(low) != tekst.charAt(high)) {
                 isPalindrome = false;
                 break;
@@ -23,5 +26,4 @@ public class Main {
         System.out.println(tekst + (isPalindrome == true ? " er" : "er ikke") + " et palindrom");
 
     }
-
 }
